dda7584bd7afc7c48a0fa35e563c577c07d5e555
Webster <websterb17@gmail.com>
2023-05-12 04:57:08 -0400

    Minor fix

--- actix_microservice/src/property.rs
+++ actix_microservice/src/property.rs
@@ -1,25 +1,3 @@
-// use actix_web::{web, HttpResponse, Responder};
-// use mysql_async::{prelude::Queryable, Pool};
-// use serde::Deserialize;
-
-// #[derive(Deserialize)]
-// pub struct Property {
-//     address: String,
-//     owner: String,
-// }
-
-// pub async fn add_property(pool: web::Data<Pool>, property: web::Json<Property>) -> impl Responder {
-//     let mut conn = pool.get_conn().await.unwrap();
-//     conn.exec_drop(
-//         "INSERT INTO properties (address, owner) VALUES (?, ?)",
-//         (&property.address, &property.owner),
-//     )
-//     .await
-//     .unwrap();
-
-//     HttpResponse::Ok().body("Property added successfully")
-// }
-
 use actix_web::{web, HttpResponse, Responder};
 use mysql_async::{prelude::Queryable, Pool};
 use serde::Deserialize;
@@ -31,13 +9,13 @@ pub struct Property {
 }
 
 pub async fn add_property(pool: web::Data<Pool>, property: web::Json<Property>) -> impl Responder {
-    let mut conn = pool.get_conn().await.expect("Failed to get connection from pool");
+    let mut conn = pool.get_conn().await.unwrap();
     conn.exec_drop(
         "INSERT INTO properties (address, owner) VALUES (?, ?)",
         (&property.address, &property.owner),
     )
     .await
-    .expect("Failed to insert property");
+    .unwrap();
 
     HttpResponse::Ok().body("Property added successfully")
 }
